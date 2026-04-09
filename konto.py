fc2178718b498caa478fdad217bdc4e4475c2eab
Nabi Hussaini <nabihusseini@gmail.com>
2026-04-09 22:51:24 +0200

    Class konto uppdatering

--- konto.py
+++ konto.py
@@ -1,13 +1,12 @@
 class Konto:
     def __init__(self, saldo=0):
         self.saldo = saldo
-        def sätt_in(self, belopp):
-            self.saldo += belopp
-            def ta_ut(self, belopp):
-                if belopp <= self.saldo:
-                    self.saldo -= belopp
-                else:
-                    print("Otillräckligt saldo")
-                    def visa_saldo(self):
-                        return self.saldo
-                    
+    def sätt_in(self, belopp):
+        self.saldo += belopp
+    def ta_ut(self, belopp):
+        if belopp <= self.saldo:
+            self.saldo -= belopp
+        else:
+            print("Otillräckligt saldo")
+    def visa_saldo(self):
+        return self.saldo
