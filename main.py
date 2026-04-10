04bc6305413e2224b1ce30e303a8f4f90ad129ba
Nabi Hussaini <nabihusseini@gmail.com>
2026-04-10 22:29:47 +0200

    Added interactive CLI menu with deposit, withdrawel, balance display, input handling and exit functionality.

--- main.py
+++ main.py
@@ -3,23 +3,27 @@ from api import hämta_valutakurs
 import json
 konto = Konto()
 
-print("1. Sätt in pengar")
-print("2. Ta ut pengar")
-print("3. Visa saldo")
-
-val = input("Välj: ")
-
-if val == "1":
-    belopp = int(input("hur mycket vill du sätta in? "))
-    konto.sätt_in(belopp)
-elif val == "2":
-    belopp = int(input("hur mycket vill du ta ut? "))
-    konto.ta_ut(belopp)
-elif val == "3":
-    print("Saldo:", konto.visa_saldo())
-
-data = {"saldo": konto.visa_saldo()}
-
-with open("data.json", "w") as f:
-    json.dump(data, f)
-
+while True:
+    print("1. Sätt in pengar")
+    print("2. Ta ut pengar")
+    print("3. Visa saldo")
+    print("4. Avsluta")
+    val = input("Välj: ")
+    if val == "1":
+        belopp = int(input("hur mycket vill du sätta in? "))
+        konto.sätt_in(belopp)
+    elif val == "2":
+        belopp = int(input("hur mycket vill du ta ut? "))
+        konto.ta_ut(belopp)
+    elif val == "3":
+        print("Saldo:", konto.visa_saldo())
+    elif val == "4":
+        print("programmet avslutas")
+        break
+    else:
+        print("Ogiltigt val")
+        
+    data = {"saldo": konto.visa_saldo()}
+    with open("data.json", "w") as f:
+     json.dump(data, f)
+     
